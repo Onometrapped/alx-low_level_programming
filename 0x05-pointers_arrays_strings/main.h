ce1d2f735221adb995c4afea9c923e9862f42cba
Onometrapped <ewomazinookupa@gmail.com>
2022-07-12 15:06:35 -0700

    Header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void swap_int(int *a, int *b);
@@ -10,3 +13,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
+#endif
